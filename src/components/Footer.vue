4df4936c98c9d7e52892663ec367bcd22c7c3c03
shanping <shanping@lattebank.com>
2025-12-24 15:31:16 +0800

    feat: 修改页脚

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-transparent py-4 text-center text-sm text-gray-600 dark:text-gray-400">
-    <div>© {{ new Date().getFullYear() }} made with ♥️ by <a class="text-purple-600" href="https://jianzhugo.cn">水常</a></div>
+    <div>© {{ new Date().getFullYear() }} made with ♥️ by shellingford</div>
     <div>已稳定运行: {{ uptime }}</div> 
   </footer>
 </template>
